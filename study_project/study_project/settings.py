34c7167bcb11909ae2ad28b8940e06e2ba2f9eb6
Shen-Teng Tu <tusamten@gmail.com>
2021-01-22 22:00:55 +0800

    ALLOWED_HOSTS setting

--- study_project/study_project/settings.py
+++ study_project/study_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&6)ao83qrak0mmeez(ji%e(+@)5(4g6a*qfz!hzi=cs&nrcdt1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
